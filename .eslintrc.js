35e8c626357d1732fad24f417b7d9685288b6976
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-05-03 00:34:30 +0300

    commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: ["@typescript-eslint/parser"],
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+};
